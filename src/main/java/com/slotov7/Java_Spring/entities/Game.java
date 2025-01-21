8f95b6e3d77119b96515dde382376e5cec0142d6
Slotov-7 <guilhermguilhermegui888@gmail.com>
2025-01-21 18:21:41 -0300

    game seed

--- src/main/java/com/slotov7/Java_Spring/entities/Game.java
+++ src/main/java/com/slotov7/Java_Spring/entities/Game.java
@@ -16,24 +16,31 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String platform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
-    private String fullDescription;
+
+    @Column(columnDefinition = "TEXT")
+    private String  longDescription;
+
 
 
 
     public Game() {
     }
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription, String fullDescription) {
+
+    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription, Double score) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platforms;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
-        this.fullDescription = fullDescription;
+        this.longDescription = longDescription;
+        this.score = score;
     }
 
     public Long getId() {
@@ -68,12 +75,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
     }
 
     public String getImgUrl() {
@@ -92,12 +99,19 @@ public class Game {
         this.shortDescription = shortDescription;
     }
 
-    public String getFullDescription() {
-        return fullDescription;
+    public String getLongDescription() {
+        return longDescription;
+    }
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
     }
 
-    public void setFullDescription(String fullDescription) {
-        this.fullDescription = fullDescription;
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 
     @Override
