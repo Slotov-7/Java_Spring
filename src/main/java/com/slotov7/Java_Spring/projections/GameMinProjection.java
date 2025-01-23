7b2991c204fe20345ccf22ee9c31c96bea900ad6
Slotov-7 <guilhermguilhermegui888@gmail.com>
2025-01-23 09:01:38 -0300

    homolog ok

--- src/main/java/com/slotov7/Java_Spring/projections/GameMinProjection.java
+++ src/main/java/com/slotov7/Java_Spring/projections/GameMinProjection.java
@@ -3,7 +3,7 @@ package com.slotov7.Java_Spring.projections;
 public interface GameMinProjection {
     Long getId();
     String getTitle();
-    Integer getYear();
+    Integer getGameYear();
     String getImgUrl();
     String getShortDescription();
     Integer getPosition();
